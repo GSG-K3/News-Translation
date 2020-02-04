5de8f2f4a5c0a0d480f893391ad1046d92a26650
Hussein-shahatet97 <hussein.sh1997@hotmail.com>
2020-02-04 15:51:04 +0200

    connection newsApi > #6

--- code.js
+++ code.js
@@ -1,14 +1,33 @@
 let menu = document.getElementById("search-section__menu")
 let search = document.getElementById("search-section__button")
 let countryCode;
+let li=document.createElement('li');
+let ul=document.getElementsByClassName("topic-item");
+
 
 search.addEventListener('click', () =>{
-     countryCode = menu.value;
-    console.log(countryCode)
+    let countryCode = menu.value;
+    let xhr = new XMLHttpRequest;
+    
+
+    xhr.onreadystatechange = () => {
+        if (xhr.status === 200 && xhr.readyState === 4) {
+            const response = JSON.parse(xhr.responseText)
+            console.log(response)
+        }
+    }
+    
+    var url = `https://newsapi.org/v2/top-headlines?country=${countryCode}&apiKey=626633f093cd40b7bda4ca1a94cc2b89`
+         console.log(url)
+
+    xhr.open('GET', url)
+    xhr.send();
 })
 
 
-var url = `https://newsapi.org/v2/top-headlines?' +
-          'country=${countryCode}&' +
-          'apiKey=626633f093cd40b7bda4ca1a94cc2b89`;
 
+apicall(url, (response) => {
+    apicall(response.title, (response) => {
+        console.log(response[0].title);
+    });
+});
